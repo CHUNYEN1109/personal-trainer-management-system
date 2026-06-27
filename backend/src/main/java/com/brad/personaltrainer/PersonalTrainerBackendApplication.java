f1d377be4aa98ce61b1af1c0be7be830063ad05c
Brad Lee <chun19971109@gmail.com>
2026-06-27 19:38:39 +0100

    Add Spring Boot backend setup

---
+++ backend/src/main/java/com/brad/personaltrainer/PersonalTrainerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brad.personaltrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTrainerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTrainerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brad/personaltrainer/PersonalTrainerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brad.personaltrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTrainerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
